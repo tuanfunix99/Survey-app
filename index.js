1e243f4e037899547eb4c03784473b46157ddf05
minggu99 <tuanphamtran99@gmail.com>
2021-07-28 11:01:24 +0700

    Server Side Architecture

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+//Dependencies
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+
+
+//Instantiate app
+const app = express();
+
+//router
+app.get('/greeting', (req, res, next) => {
+    res.send({hi: 'hello'});
+})
+
+
+//listen server
+app.listen(PORT, () => {
+    console.log('Listening on port ' + PORT);
+})
