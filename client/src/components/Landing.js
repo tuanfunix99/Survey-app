e3975bd1e3c4d423c6a1ceec514f580b374a5c15
Minggu99 <tuanphamtran99@gmail.com>
2021-09-16 13:26:44 +0700

    update feature add survey

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
     <React.Fragment>
       <h1>Survey!</h1>
       <p>welcome to the survey app</p>
+      <div className="fixed-action-btn">
+        <Link to="/surveys" className="btn-floating btn-large red">
+          <i className="large material-icons">assessment</i>
+        </Link>
+      </div>
     </React.Fragment>
   );
 };
